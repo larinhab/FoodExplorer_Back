ff3766f906d378dc1b9779fc16a5281c7d0410ef
larinhab <larabviana@outlook.com>
2024-05-20 09:37:07 -0300

    :new: first commit

---
+++ src/server.js
@@ -0,0 +1,34 @@
+require("express-async-errors") // IMPORTANDO O ASYNC ERRORS
+
+const express = require("express") // IMPORTANDO EXPRESS
+const app = express() // INICIALIZANDO EXPRESS 
+const cors = require("cors") // POSSIBILITA QUE O BACK ATENDA O FRONT
+
+const req = require("express/lib/request")
+app.use(express.json()) // PADRÃO DAS REQUISIÇÕES
+
+const routes = require("./routes") 
+app.use(cors()) // COMPARTILHAMENTO DE RECURSOS
+app.use(routes)
+
+// DEFININDO A PORTA
+const PORT = process.env.PORT || 2008
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+
+// ADICIONANDO O APP ERROR COMO PADRÃO DE ERROS
+const AppError = require("./utilis/AppError.js")
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status:"error",
+            message: error.message
+        })
+    }
+    console.log(error)
+
+    return response.status(500).json({
+        status:"error",
+        message: "Server Internal Error"
+    })
+});
