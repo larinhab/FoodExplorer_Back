330293cd52fee034a55cca3da668c153d1f99c8d
larinhab <larabviana@outlook.com>
2024-05-21 08:46:06 -0300

    :new: add jwt auth

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utilis/AppError")
 const knex = require("../database/knex")
-const { hash, compare } = require("bcrypt")
+const { hash, compare } = require("bcryptjs")
 
 class UsersController {
     async create(request, response){
@@ -8,6 +8,10 @@ class UsersController {
 
         const cryptPassword = await hash(password, 8)
         const checkIfEmailExist = await knex("users").where({ email }).first()
+
+        if (checkIfEmailExist){
+            throw new AppError("E-mail já em uso", 400)
+        }
 
         await knex("users").insert({
             name,
@@ -17,6 +21,28 @@ class UsersController {
 
         return response.json({
             message: "Usuário criado com sucesso!"
+        }, 201)
+    }
+
+    async uptade(request, response){
+        const { name, email, password, oldPassword } = request.body
+        const user_id = request.user.id
+
+        console.log(user_id)
+
+        const user = await knex("users").where({ id: user_id}).first()
+        
+        await knex("users").where({ id: user_id }).update({
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            uptade_at: knex.fn.now()
+        })
+
+        return response.status(200).json({
+            name: user.name,
+            email: user.email,
+            message: "Usuário alterado com sucesso"
         })
     }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router } = require("express")
+
 const usersRoutes = require("./users.routes")
+const sessionsRoutes = require("./sessions.routes")
 
 const routes = Router()
 
+routes.use("/sessions", sessionsRoutes)
 routes.use("/users", usersRoutes)
 
 module.exports = routes
